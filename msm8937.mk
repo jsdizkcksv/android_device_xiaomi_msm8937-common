c43437dfe715e509e6edb5deeb8d9a964b1e65df
Quallenauge <Hamsi2k@freenet.de>
2020-11-16 22:16:17 +0100

    ulysse: vibrator: Switch to aidl vibrator hal.
    Change-Id: I4dec22449f33bdcd6170759075bdcec5bb715031

--- msm8937.mk
+++ msm8937.mk
@@ -387,11 +387,11 @@ PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
 # Vibrator
-ifneq ($(TARGET_USES_DEVICE_SPECIFIC_VIBRATOR),true)
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
-endif
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_COPY_FILES += \
